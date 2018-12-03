61989f4e8c258c0becc7b8ee80adfebc64ebb0e0
Emanuele <emanuelegorga@gmail.com>
2018-12-03 17:25:13 +0000

    Step 6 - completed with exceed limit method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,19 @@ class Oystercard
 
   attr_reader :balance
 
+  LIMIT = 90
+
   def initialize(balance = 0)
     @balance = balance
   end
 
   def topup(money)
+    raise 'Sorry, limit exceeded!' if exceed_limit == true
     @balance += money
+  end
+
+  def exceed_limit
+    @balance >= LIMIT ? true : false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,8 @@ require "oystercard"
 
 describe Oystercard do
 
+  let(:card) { Oystercard.new }
+
   it { is_expected.to respond_to(:balance) }
 
   it "checks that initialized balance is 0" do
@@ -12,10 +14,26 @@ describe Oystercard do
 
   it  { is_expected.to respond_to(:topup).with(1).argument }
 
-  it "tops up the current balance of the card with whatever value is passed as an argument" do
-    expect{ subject.topup(1) }.to change{ subject.balance }.by(1)
+    it "tops up the current balance of the card with whatever value is passed as an argument" do
+      expect{ subject.topup(1) }.to change{ subject.balance }.by(1)
+    end
+
+    it "throws an error if the limit is exceeded" do
+      allow(card).to receive(:exceed_limit).and_return(true)
+      expect{ card.topup(10) }.to raise_error "Sorry, limit exceeded!"
+    end
+
   end
 
-end
+  it { is_expected.to respond_to(:exceed_limit) }
+
+  context "#exceed_limit" do
+
+    it "returns true or false" do
+      expect(card.exceed_limit).to be(true).or be(false)
+    end
+
+  end
+
 
 end
