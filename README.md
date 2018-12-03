16fa97d67f26e20a9e0428335a7fedb5f73ad43f
Emanuele <emanuelegorga@gmail.com>
2018-12-03 15:33:10 +0000

    Step 4 - first test passed for balance method

---
+++ README.md
@@ -0,0 +1,10 @@
+# Oyster card project
+
+Weekly project for our second week at Makers Academy.(br/)
+Developed using pairing programming.
+
+## How to use ##
+
+```
+ruby oystercard.rb
+```
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,10 @@ require "oystercard"
 
 describe Oystercard do
 
+  it { is_expected.to respond_to(:balance) }
+
+  it "checks that initialized balance is 0" do
+    expect(subject.balance).to eq(0)
+  end
+
 end
